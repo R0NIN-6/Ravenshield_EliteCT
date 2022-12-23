75dd5d2982085a5995ca9dea10a5049d155b5185
R0NIN-6 <44343527+R0NIN-6@users.noreply.github.com>
2022-12-23 22:50:03 +0000

    Add files via upload

---
+++ BulkEdit-TemplateFiles.ps1
@@ -0,0 +1,20 @@
+﻿#add this to all MP* files with a random distribution of numbers
+$EnemyString = "NbOfPawn=18
+010, Mercs.Merc1
+005, Mercs.Merc2
+005, Mercs.Merc3
+005, Mercs.Merc4
+010, Mercs.Merc5
+005, Mercs.Merc6
+005, Mercs.Merc7
+005, Mercs.Merc8
+005, Mercs.Merc9
+005, Mercs.Merc10
+005, Mercs.Merc11
+005, Mercs.Merc12
+005, Mercs.MercGM
+005, Mercs.MercGM2
+005, Mercs.MercGM3
+"
+
+$EnemyTptFiles = gci -Path 'C:\Rainbow Six 3\Future\Mods\EliteCounterTerror\template' -Filter *tpt
