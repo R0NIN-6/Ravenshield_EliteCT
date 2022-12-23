0c4601c0d424b179aff4095d9407232d5cec7804
R0NIN-6 <44343527+R0NIN-6@users.noreply.github.com>
2022-12-23 17:08:27 -0600

    Update BulkEdit-TemplateFiles.ps1

--- BulkEdit-TemplateFiles.ps1
+++ BulkEdit-TemplateFiles.ps1
@@ -1,20 +1,21 @@
 ﻿#add this to all MP* files with a random distribution of numbers
 $EnemyString = "NbOfPawn=18
-010, Mercs.Merc1
-005, Mercs.Merc2
-005, Mercs.Merc3
-005, Mercs.Merc4
-010, Mercs.Merc5
-005, Mercs.Merc6
-005, Mercs.Merc7
-005, Mercs.Merc8
-005, Mercs.Merc9
-005, Mercs.Merc10
-005, Mercs.Merc11
-005, Mercs.Merc12
-005, Mercs.MercGM
-005, Mercs.MercGM2
-005, Mercs.MercGM3
+010, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+010, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
+005, Enter enemy type (package.name)
 "
 
-$EnemyTptFiles = gci -Path 'C:\Rainbow Six 3\Future\Mods\EliteCounterTerror\template' -Filter *tpt
+$EnemyTptFiles = gci -Path 'C:\Rainbow Six 3\Future\Mods\EliteCounterTerror\template' -recurse -file -Filter *tpt
+#todo find correct placement within file and replace text
