c300b1b3326cc045f9ef0da8b2364bf7a5583818
R0NIN-6 <44343527+R0NIN-6@users.noreply.github.com>
2023-01-08 20:10:33 -0600

    Added folder structure
    Added Folder structure and Strip batch file.

---
+++ Tools/Move-SoundFiles.ps1
@@ -0,0 +1,4 @@
+﻿$RavenshieldPath = "enter full path to Ravenshield install"
+$AthenaSwordSound = get-childitem -Path "$RavenshieldPath\mods\athenasword\sounds\" -Recurse -Directory
+$IronWrathSound = get-childitem -Path "$RavenshieldPath\mods\ironwrath\sounds\" -Recurse -Directory
+Move-Item $AthenaSwordSound,$IronWrathSound -Destination "$Ravenshield\sounds" -Force
---
+++ Tools/RvSBulkImageConvert.ps1
@@ -0,0 +1,19 @@
+﻿$Inputdirectory = "<path to UE Viewer>\UmodelExport\\"
+$OutputDirectory = "<full path>\ConvertedPNGs"
+$TextureSubFolders = gci -Path $Inputdirectory -Recurse -Directory -Filter Texture | select fullname
+$strFolder =""
+$folderName =""
+foreach($folder in $TextureSubFolders)
+{
+    #$folder = 
+    $strFolder =$folder.FullName
+    Write-Output "full path: $strFolder"
+    $folderName= $strFolder.TrimStart($Inputdirectory)
+    set-location $strFolder
+    #Start-Process -Wait -PassThru -filepath 'c:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe' -argumentlist " mogrify -format png *.tga"
+    #Move-Item -Path .\ -Filter *.png -Destination "$OutputDirectory\$folderName" -Force
+    Write-Output "Folder name for out: $folderName"
+  # ls
+    $strFolder =""
+    $folderName=""
+}
